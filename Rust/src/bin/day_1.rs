34cacc8074b2bf060d609672f94d1b780f6b5062
maddenvvs <maddenvvs@gmail.com>
2020-12-01 19:55:11 +0400

    Add Rust solution for Day 1

---
+++ Rust/src/bin/day_1.rs
@@ -0,0 +1,67 @@
+use std::collections::HashSet;
+use std::fs;
+
+const NEW_YEAR: i32 = 2020;
+
+fn find_product_of_three_numbers_equal_to_2020(entities: &Vec<i32>) -> i32 {
+    let mut entities_copy = entities.to_vec();
+    entities_copy.sort();
+
+    for f in 0..(entities.len() - 2) {
+        let target_sum = NEW_YEAR - entities_copy[f];
+        if target_sum <= 0 {
+            continue;
+        }
+
+        let mut l = f + 1;
+        let mut r = entities.len() - 1;
+
+        while l < r {
+            let temp_sum = entities_copy[l] + entities_copy[r];
+
+            if temp_sum == target_sum {
+                return entities_copy[f] * entities_copy[l] * entities_copy[r];
+            }
+
+            if temp_sum < target_sum {
+                l += 1;
+            } else {
+                r -= 1;
+            }
+        }
+    }
+
+    return 0;
+}
+
+fn find_product_of_two_numbers_equal_to_2020(entities: &Vec<i32>) -> i32 {
+    let mut seen = HashSet::new();
+
+    for entity in entities {
+        let candidate = NEW_YEAR - entity;
+        if seen.contains(&candidate) {
+            return candidate * entity;
+        }
+
+        seen.insert(entity);
+    }
+
+    return 0;
+}
+
+fn main() {
+    let other = fs::read_to_string("input/day-1.input").unwrap();
+    let content: Vec<i32> = other
+        .split_whitespace()
+        .map(|s| s.parse().unwrap())
+        .collect();
+
+    println!(
+        "Day 1-1: {}",
+        find_product_of_two_numbers_equal_to_2020(&content)
+    );
+    println!(
+        "Day 1-2: {}",
+        find_product_of_three_numbers_equal_to_2020(&content)
+    );
+}
