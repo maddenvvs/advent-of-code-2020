e0fe509397729deb99c51bf87af9c4faba93b8cf
maddenvvs <maddenvvs@gmail.com>
2020-12-04 22:43:42 +0400

    Refactor Rust day 2 to using structs

--- Rust/src/bin/day_2.rs
+++ Rust/src/bin/day_2.rs
@@ -8,42 +8,69 @@ lazy_static! {
     static ref PASSWORD_PATTERN: Regex = Regex::new(r"^(\d+)-(\d+) (\w): (\w+)$").unwrap();
 }
 
-fn current_policy(first_number: i32, second_number: i32, letter: char, password: &str) -> bool {
-    return (password.chars().nth((first_number - 1) as usize).unwrap() == letter)
-        ^ (password.chars().nth((second_number - 1) as usize).unwrap() == letter);
+struct PasswordDefinition<'a> {
+    first_number: i32,
+    second_number: i32,
+    letter: char,
+    password: &'a str,
 }
 
-fn old_policy(first_number: i32, second_number: i32, letter: char, password: &str) -> bool {
-    let occurences_of_letter = password.chars().filter(|&ch| ch == letter).count() as i32;
+impl PasswordDefinition<'_> {
+    fn from(password_text: &str) -> PasswordDefinition {
+        let captures = PASSWORD_PATTERN.captures(password_text).unwrap();
 
-    first_number <= occurences_of_letter && occurences_of_letter <= second_number
-}
+        let first_number: i32 = captures.get(1).unwrap().as_str().parse().unwrap();
+        let second_number: i32 = captures.get(2).unwrap().as_str().parse().unwrap();
+        let letter: char = captures.get(3).unwrap().as_str().chars().next().unwrap();
+        let password: &str = captures.get(4).unwrap().as_str();
 
-fn is_password_valid(
-    password: &str,
-    password_policy: &dyn Fn(i32, i32, char, &str) -> bool,
-) -> bool {
-    let captures = PASSWORD_PATTERN.captures(password).unwrap();
+        PasswordDefinition {
+            first_number,
+            second_number,
+            letter,
+            password,
+        }
+    }
 
-    let first_number: i32 = captures.get(1).unwrap().as_str().parse().unwrap();
-    let second_number: i32 = captures.get(2).unwrap().as_str().parse().unwrap();
-    let letter: char = captures.get(3).unwrap().as_str().chars().next().unwrap();
-    let password_text: &str = captures.get(4).unwrap().as_str();
+    fn align_with_old_policy(&self) -> bool {
+        let occurences_of_letter = self
+            .password
+            .chars()
+            .filter(|&ch| ch == self.letter)
+            .count() as i32;
 
-    password_policy(first_number, second_number, letter, password_text)
+        self.first_number <= occurences_of_letter && occurences_of_letter <= self.second_number
+    }
+
+    fn align_with_current_policy(&self) -> bool {
+        (self
+            .password
+            .chars()
+            .nth((self.first_number - 1) as usize)
+            .unwrap()
+            == self.letter)
+            ^ (self
+                .password
+                .chars()
+                .nth((self.second_number - 1) as usize)
+                .unwrap()
+                == self.letter)
+    }
 }
 
 fn count_old_valid_passwords(passwords: &[&str]) -> i32 {
     return passwords
         .iter()
-        .filter(|password| is_password_valid(password, &old_policy))
+        .map(|password| PasswordDefinition::from(password))
+        .filter(|def| def.align_with_old_policy())
         .count() as i32;
 }
 
 fn count_current_valid_passwords(passwords: &[&str]) -> i32 {
     return passwords
         .iter()
-        .filter(|password| is_password_valid(password, &current_policy))
+        .map(|password| PasswordDefinition::from(password))
+        .filter(|def| def.align_with_current_policy())
         .count() as i32;
 }
 
