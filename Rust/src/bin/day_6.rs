a6af028327ba8f2d92f76832104d03db3e3a04b3
maddenvvs <maddenvvs@gmail.com>
2020-12-06 11:58:39 +0400

    Add solution for day 6 in Rust

---
+++ Rust/src/bin/day_6.rs
@@ -0,0 +1,83 @@
+use std::collections::HashSet;
+use std::fs;
+
+type Group<'a> = Vec<&'a str>;
+
+fn parse_answers(answers_text: &str) -> Vec<Group> {
+    answers_text
+        .split("\n\n")
+        .map(|g| g.split_whitespace().collect())
+        .collect()
+}
+
+fn sum_of_unique_answers(group: &Group) -> i32 {
+    group
+        .iter()
+        .fold(&mut HashSet::with_capacity(26), |acc, el| {
+            acc.extend(el.chars());
+            acc
+        })
+        .len() as i32
+}
+
+fn sum_of_common_answers(group: &Group) -> i32 {
+    let unique_answers: HashSet<char> = "abcdefghijklmnopqrstuvwxyz".chars().collect();
+
+    group
+        .iter()
+        .fold(unique_answers, |acc, el| {
+            let mut result: HashSet<char> = HashSet::new();
+            let chars_set: HashSet<char> = el.chars().collect::<HashSet<char>>();
+
+            for el in acc.intersection(&chars_set) {
+                result.insert(*el);
+            }
+
+            result
+        })
+        .len() as i32
+}
+
+fn total_sum_of_unique_answers(groups: &[Group]) -> i32 {
+    groups.iter().map(sum_of_unique_answers).sum()
+}
+
+fn total_sum_of_common_answers(groups: &[Group]) -> i32 {
+    groups.iter().map(sum_of_common_answers).sum()
+}
+
+fn test_tasks() {
+    let test_answers = "abc
+
+a
+b
+c
+
+ab
+ac
+
+a
+a
+a
+a
+
+b";
+
+    let parsed_answers = parse_answers(test_answers);
+
+    assert_eq!(total_sum_of_unique_answers(&parsed_answers), 11);
+    assert_eq!(total_sum_of_common_answers(&parsed_answers), 6);
+}
+
+fn run_tasks() {
+    let answers_text = fs::read_to_string("input/day-6.input").expect("Missing answers file");
+    let parsed_answers = parse_answers(&answers_text);
+
+    println!("Day 6-1: {}", total_sum_of_unique_answers(&parsed_answers));
+    println!("Day 6-2: {}", total_sum_of_common_answers(&parsed_answers));
+}
+
+fn main() {
+    test_tasks();
+    run_tasks();
+}
