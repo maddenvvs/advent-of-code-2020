625a363ed26dc6c416004431bffa8e8cd859c27b
maddenvvs <maddenvvs@gmail.com>
2020-12-24 12:54:29 +0400

    Add solution in Rust for day 24

--- Rust/src/lib.rs
+++ Rust/src/lib.rs
@@ -36,7 +36,7 @@ fn available_solutions() -> Vec<Box<dyn solution::Solution>> {
         Box::new(day_21::Day21 {}),
         Box::new(day_22::Day22 {}),
         Box::new(day_23::Day23 {}),
-        Box::new(NoSolution),
+        Box::new(day_24::Day24 {}),
         Box::new(NoSolution),
     ]
 }
---
+++ Rust/src/solutions/day_24.rs
@@ -0,0 +1,235 @@
+use super::solution::{Error, Solution};
+use num_complex::Complex;
+use std::collections::{HashMap, HashSet};
+
+type Point = Complex<i32>;
+
+fn add_point(neighbours: &mut HashMap<Point, usize>, point: Point) {
+    for offset in &[
+        Point::new(-2, 0),
+        Point::new(2, 0),
+        Point::new(-1, -3),
+        Point::new(-1, 3),
+        Point::new(1, -3),
+        Point::new(1, 3),
+    ] {
+        *neighbours.entry(point + offset).or_insert(0) += 1;
+    }
+}
+
+fn result_position(instructions: &str) -> Point {
+    let mut position = Point::new(0, 0);
+    let mut idx: usize = 0;
+    let chars: Vec<char> = instructions.chars().collect();
+
+    while idx < chars.len() {
+        match chars[idx] {
+            'e' => {
+                position += Point::new(-2, 0);
+                idx += 1;
+            }
+            'w' => {
+                position += Point::new(2, 0);
+                idx += 1;
+            }
+            'n' => {
+                if chars[idx + 1] == 'e' {
+                    position += Point::new(-1, 3);
+                } else {
+                    position += Point::new(1, 3);
+                }
+                idx += 2;
+            }
+            's' => {
+                if chars[idx + 1] == 'e' {
+                    position += Point::new(-1, -3);
+                } else {
+                    position += Point::new(1, -3);
+                }
+                idx += 2;
+            }
+            i => panic!("Unsupported instruction: {}", i),
+        };
+    }
+
+    position
+}
+
+struct TileFloor {
+    floor: HashSet<Point>,
+    neighbours: HashMap<Point, usize>,
+    buffer: HashSet<Point>,
+    buffer_neighbours: HashMap<Point, usize>,
+}
+
+impl TileFloor {
+    fn from_instructions(instructions_text: &str) -> TileFloor {
+        let floor = instructions_text
+            .lines()
+            .map(result_position)
+            .fold(&mut HashMap::new(), |acc, el| {
+                *acc.entry(el).or_insert(0) += 1;
+                acc
+            })
+            .iter()
+            .filter(|(_, &c)| c % 2 == 1)
+            .map(|(p, _)| p)
+            .copied()
+            .collect::<HashSet<_>>();
+
+        let mut neighbours: HashMap<Point, usize> = HashMap::new();
+        for &point in floor.iter() {
+            add_point(&mut neighbours, point);
+        }
+
+        TileFloor {
+            floor,
+            neighbours,
+            buffer: HashSet::new(),
+            buffer_neighbours: HashMap::new(),
+        }
+    }
+
+    fn black_tiles(&self) -> usize {
+        self.floor.len()
+    }
+
+    fn simulate_day(&mut self) -> usize {
+        let mut black_tiles = 0;
+        self.buffer.clear();
+        self.buffer_neighbours.clear();
+
+        for (&point, &active_neighbours) in self.neighbours.iter() {
+            let is_black = self.floor.contains(&point);
+            if (is_black && 1 <= active_neighbours && active_neighbours <= 2)
+                || (!is_black && active_neighbours == 2)
+            {
+                black_tiles += 1;
+
+                self.buffer.insert(point);
+                add_point(&mut self.buffer_neighbours, point);
+            }
+        }
+
+        std::mem::swap(&mut self.floor, &mut self.buffer);
+        std::mem::swap(&mut self.neighbours, &mut self.buffer_neighbours);
+        black_tiles
+    }
+}
+
+fn black_tiles_after_n_days(tile_floor: &mut TileFloor, days: usize) -> usize {
+    (0..days).fold(tile_floor.black_tiles(), |_, _| tile_floor.simulate_day())
+}
+
+pub struct Day24 {}
+
+impl Solution for Day24 {
+    fn first_task(&self, instructions_text: &str) -> Result<String, Error> {
+        Ok(TileFloor::from_instructions(instructions_text)
+            .black_tiles()
+            .to_string())
+    }
+
+    fn second_task(&self, instructions_text: &str) -> Result<String, Error> {
+        let mut tile_floor = TileFloor::from_instructions(instructions_text);
+
+        Ok(black_tiles_after_n_days(&mut tile_floor, 100).to_string())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_result_position() {
+        assert_eq!(result_position("nwwswee"), Point::new(0, 0));
+    }
+
+    #[test]
+    fn test_first_task() {
+        let test_instructions = "sesenwnenenewseeswwswswwnenewsewsw
+neeenesenwnwwswnenewnwwsewnenwseswesw
+seswneswswsenwwnwse
+nwnwneseeswswnenewneswwnewseswneseene
+swweswneswnenwsewnwneneseenw
+eesenwseswswnenwswnwnwsewwnwsene
+sewnenenenesenwsewnenwwwse
+wenwwweseeeweswwwnwwe
+wsweesenenewnwwnwsenewsenwwsesesenwne
+neeswseenwwswnwswswnw
+nenwswwsewswnenenewsenwsenwnesesenew
+enewnwewneswsewnwswenweswnenwsenwsw
+sweneswneswneneenwnewenewwneswswnese
+swwesenesewenwneswnwwneseswwne
+enesenwswwswneneswsenwnewswseenwsese
+wnwnesenesenenwwnenwsewesewsesesew
+nenewswnwewswnenesenwnesewesw
+eneswnwswnwsenenwnwnwwseeswneewsenese
+neswnwewnwnwseenwseesewsenwsweewe
+wseweeenwnesenwwwswnew";
+
+        assert_eq!(
+            TileFloor::from_instructions(test_instructions).black_tiles(),
+            10
+        );
+    }
+
+    #[test]
+    fn test_simulate_day() {
+        let test_instructions = "sesenwnenenewseeswwswswwnenewsewsw
+neeenesenwnwwswnenewnwwsewnenwseswesw
+seswneswswsenwwnwse
+nwnwneseeswswnenewneswwnewseswneseene
+swweswneswnenwsewnwneneseenw
+eesenwseswswnenwswnwnwsewwnwsene
+sewnenenenesenwsewnenwwwse
+wenwwweseeeweswwwnwwe
+wsweesenenewnwwnwsenewsenwwsesesenwne
+neeswseenwwswnwswswnw
+nenwswwsewswnenenewsenwsenwnesesenew
+enewnwewneswsewnwswenweswnenwsenwsw
+sweneswneswneneenwnewenewwneswswnese
+swwesenesewenwneswnwwneseswwne
+enesenwswwswneneswsenwnewswseenwsese
+wnwnesenesenenwwnenwsewesewsesesew
+nenewswnwewswnenesenwnesewesw
+eneswnwswnwsenenwnwnwwseeswneewsenese
+neswnwewnwnwseenwseesewsenwsweewe
+wseweeenwnesenwwwswnew";
+
+        let mut test_floor = TileFloor::from_instructions(test_instructions);
+
+        for &black_tiles in &[15, 12, 25, 14, 23, 28, 41, 37, 49, 37] {
+            assert_eq!(test_floor.simulate_day(), black_tiles as usize);
+        }
+    }
+
+    #[test]
+    fn test_second_task() {
+        let test_instructions = "sesenwnenenewseeswwswswwnenewsewsw
+neeenesenwnwwswnenewnwwsewnenwseswesw
+seswneswswsenwwnwse
+nwnwneseeswswnenewneswwnewseswneseene
+swweswneswnenwsewnwneneseenw
+eesenwseswswnenwswnwnwsewwnwsene
+sewnenenenesenwsewnenwwwse
+wenwwweseeeweswwwnwwe
+wsweesenenewnwwnwsenewsenwwsesesenwne
+neeswseenwwswnwswswnw
+nenwswwsewswnenenewsenwsenwnesesenew
+enewnwewneswsewnwswenweswnenwsenwsw
+sweneswneswneneenwnewenewwneswswnese
+swwesenesewenwneswnwwneseswwne
+enesenwswwswneneswsenwnewswseenwsese
+wnwnesenesenenwwnenwsewesewsesesew
+nenewswnwewswnenesenwnesewesw
+eneswnwswnwsenenwnwnwwseeswneewsenese
+neswnwewnwnwseenwseesewsenwsweewe
+wseweeenwnesenwwwswnew";
+
+        let mut test_floor = TileFloor::from_instructions(test_instructions);
+
+        assert_eq!(black_tiles_after_n_days(&mut test_floor, 100), 2208);
+    }
+}
